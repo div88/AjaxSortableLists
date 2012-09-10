e34d848c1d702b9a0e7eb79018ae8a1d70165951
Divya <diviyengar@gmail.com>
2012-09-10 16:01:43 -0700

    Ajax sortable list first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxBlog::Application.config.secret_token = 'ae935fc68a8037e5f0c0095cc8019bd18d886c315101547721880b6ebef82a30526ff59712770a214c56681fa9690284d6dcca68b8a25afecce6cc75156650b6'
